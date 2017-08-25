1df69f5e32262f0f6015e12016ca5bf8cf48d9d9
Karel Sommer <karel@easycoreapps.com>
2017-08-25 20:46:01 +0200

    first test

---
+++ moment.js
@@ -0,0 +1,32 @@
+/**
+ * Example template tag that generates a random number 
+ * between a user-provided MIN and MAX
+ */
+module.exports.templateTags = [{
+    name: 'future',
+    displayName: 'Future Timestamp',
+    description: 'Return Future Timestamp',
+    args: [
+        {
+            displayName: 'Number',
+            description: 'Amount',
+            type: 'number',
+            defaultValue: 1
+        }, 
+        {
+            displayName: 'Unit',
+            description: 'Time/Date unit',
+            type: 'string',
+            defaultValue: 'hour'
+        }, 
+        {
+            displayName: 'Format',
+            description: 'Time/Date format',
+            type: 'string',
+            defaultValue: null
+        }
+    ],
+    async run (context, number, unit, format) {
+        return moment().add(number,unit).format(format);
+    }
+}];
